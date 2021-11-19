04026238689ed4355c736fad9085662f4eb6848e
FusRoman <roman.lemontagner@gmail.com>
2021-11-19 18:29:00 +0100

    add continuous integration testing

--- alert_association/first_method/continuous_integration.py
+++ alert_association/first_method/continuous_integration.py
@@ -7,6 +7,8 @@ from intra_night_association import get_n_last_observations_from_trajectories
 from night_to_night_association import night_to_night_association
 import matplotlib.pyplot as plt
 import astropy.units as u
+from pandas.testing import assert_frame_equal
+import test_sample as ts
 
 
 if __name__ == "__main__":
@@ -22,33 +24,23 @@ if __name__ == "__main__":
     df_sso = df_sso.drop_duplicates(["candid"])
     df_sso = df_sso[df_sso["fink_class"] == "Solar System MPC"]
 
-    # "1951", "53317", 
-    specific_mpc = df_sso[df_sso["ssnamenr"].isin(["1951", "53317","80343"])]
+    # "1951", "53317",
 
-    print(specific_mpc)
+    # "1584" : exemple problématique avec une time window de 14 "53317", "1951", "80343", "1196", "23101",
 
-    mpc_plot = specific_mpc.groupby(['ssnamenr']).agg({
-        "ra": list,
-        "dec": list
-    }).reset_index()
+    #  & (df_sso['nid'] >= 1610) & (df_sso['nid'] <= 1624)
+    specific_mpc = df_sso[
+        (df_sso["ssnamenr"].isin(["53317", "1951", "80343", "1196", "23101", "1758"]))
+    ]
 
-    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
-
-    for _, rows in mpc_plot.iterrows():
-        ra = rows['ra']
-        dec = rows['dec']
-        label = rows['ssnamenr']
-
-        ax.scatter(ra, dec, label=label)
-
-    plt.legend()
-    plt.show()
+    mpc_plot = (
+        specific_mpc.groupby(["ssnamenr"]).agg({"ra": list, "dec": list}).reset_index()
+    )
 
     all_night = np.unique(specific_mpc["nid"])
 
     n1 = all_night[0]
     df_night1 = specific_mpc[specific_mpc["nid"] == n1]
-
 
     left, right, _ = intra_night_association(df_night1)
 
@@ -56,7 +48,7 @@ if __name__ == "__main__":
     traj_df = new_trajectory_id_assignation(left, right, last_trajectory_id)
     traj_df = traj_df.reset_index(drop=True)
 
-    if len(traj_df) > 0:
+    if len(traj_df) > 0:  # pragma: no cover
         old_observation = pd.concat(
             [df_night1[~df_night1["candid"].isin(traj_df["candid"])]]
         )
@@ -65,7 +57,7 @@ if __name__ == "__main__":
     else:
         old_observation = df_night1
 
-    time_window_limit = 12
+    time_window_limit = 14
     verbose = False
 
     for i in range(1, len(all_night)):
@@ -74,7 +66,7 @@ if __name__ == "__main__":
 
         df_next_night = specific_mpc[specific_mpc["nid"] == new_night]
 
-        if verbose:
+        if verbose:  # pragma: no cover
             print()
             print()
             print("incoming night : {}".format(new_night))
@@ -97,7 +89,7 @@ if __name__ == "__main__":
                 last_obs_of_all_traj["diff_nid"] <= time_window_limit
             ]
 
-            if verbose:
+            if verbose:  # pragma: no cover
                 print(
                     "nb most recent traj to associate : {}".format(
                         len(most_recent_last_obs)
@@ -121,16 +113,17 @@ if __name__ == "__main__":
             old_observation,
             df_next_night,
             last_trajectory_id,
-            intra_night_sep_criterion=2 * u.degree,
-            sep_criterion=2 * u.degree,
-            mag_criterion_same_fid=0.18,
-            mag_criterion_diff_fid=0.7,
-            angle_criterion=8.8,
+            intra_night_sep_criterion=10 * u.arcminute,
+            sep_criterion=1 * u.degree,
+            mag_criterion_same_fid=0.6,
+            mag_criterion_diff_fid=0.85,
+            angle_criterion=15,
+            run_intra_night_metrics=True,
         )
 
         traj_df = pd.concat([traj_df, oldest_traj])
 
-        if verbose:
+        if verbose:  # pragma: no cover
             print()
             print("elapsed time: {}".format(t.time() - t_before))
             print()
@@ -139,47 +132,86 @@ if __name__ == "__main__":
                     len(traj_df), len(old_observation)
                 )
             )
-            print("-----------------------------------------------")       
+            print("-----------------------------------------------")
 
+    with open("Output.txt", "w") as text_file:
+        import json
 
-    print(len(specific_mpc))
+        text_file.write(json.dumps(traj_df.to_dict(orient="list")))
 
-    print(len(traj_df))
+    all_alert_not_associated = specific_mpc[
+        ~specific_mpc["candid"].isin(traj_df["candid"])
+    ]
 
-    print(traj_df)
+    assert_frame_equal(
+        traj_df.reset_index(drop=True), ts.traj_df_expected, check_dtype=False
+    )
 
-    print()
-    print("show the alert not in traj_df")
-    print(specific_mpc[~specific_mpc['candid'].isin(traj_df['candid'])])
-    print()
+    assert_frame_equal(
+        all_alert_not_associated,
+        pd.DataFrame(
+            columns=[
+                "ra",
+                "dec",
+                "ssnamenr",
+                "jd",
+                "fid",
+                "nid",
+                "fink_class",
+                "objectId",
+                "candid",
+                "magpsf",
+                "sigmapsf",
+                "magnr",
+                "sigmagnr",
+                "magzpsci",
+                "isdiffpos",
+                "dcmag",
+                "dcmagerr",
+            ]
+        ),
+        check_index_type=False,
+        check_dtype=False,
+    )
 
-    gb_traj = traj_df.groupby(['trajectory_id']).agg({
-        "ra": list,
-        "dec": list,
-        "nid": list
-    }).reset_index()
+    show_results = False
 
+    if show_results:  # pragma: no cover
 
-    print()
-    print()
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(gb_traj)
+        gb_traj = (
+            traj_df.groupby(["trajectory_id"])
+            .agg(
+                {
+                    "ra": list,
+                    "dec": list,
+                    "dcmag": list,
+                    "fid": list,
+                    "nid": list,
+                    "candid": lambda x: len(x),
+                }
+            )
+            .reset_index()
+        )
 
-    print()
-    print()
-    print(gb_traj['nid'][0])
-    print()
-    print()
-    print(gb_traj['nid'][1])
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 20))
 
-    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+        for _, rows in mpc_plot.iterrows():
+            ra = rows["ra"]
+            dec = rows["dec"]
+            label = rows["ssnamenr"]
 
-    for _, rows in gb_traj.iterrows():
-        ra = rows['ra']
-        dec = rows['dec']
-        label = rows['trajectory_id']
+            ax1.scatter(ra, dec, label=label)
 
-        ax.scatter(ra, dec, label=label)
+        ax1.legend(title="mpc name")
 
-    plt.legend()
-    plt.show()
+        for _, rows in gb_traj.iterrows():
+            ra = rows["ra"]
+            dec = rows["dec"]
+            label = rows["trajectory_id"]
+            ax2.scatter(ra, dec, label=label)
+
+        ax1.set_title("real trajectories")
+        ax2.set_title("detected trajectories")
+
+        ax2.legend(title="trajectory identifier")
+        plt.show()
