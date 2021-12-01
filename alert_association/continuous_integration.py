fix a bug from get_n_last_observations_from_trajectories function, add a sort with jd for the return dataframe to perform a right angle computation

alert_association/continuous_integration.py
@@ -19,7 +19,7 @@ def load_data(path, object_class):
 
     # load all data
     for i in range(3, 7):
-        df_sso = pd.read_pickle(data_path + str(i))
+        df_sso = pd.read_pickle(path + str(i))
         all_df.append(df_sso)
 
     df_sso = pd.concat(all_df).sort_values(["jd"]).drop_duplicates()
@@ -130,13 +130,6 @@ def time_window_management(
             last_obs_of_all_traj["diff_nid"] <= time_window
         ]
 
-        if verbose:  # pragma: no cover
-            print(
-                "nb most recent traj to associate : {}".format(
-                    len(most_recent_last_obs)
-                )
-            )
-
         mask_traj = trajectory_df["trajectory_id"].isin(
             most_recent_last_obs["trajectory_id"]
         )
@@ -144,7 +137,7 @@ def time_window_management(
         most_recent_traj = trajectory_df[mask_traj]
         oldest_traj = trajectory_df[~mask_traj]
 
-    diff_nid_old_observation = current_night_id - old_observation["nid"]
+    diff_nid_old_observation = nid_next_night - old_observation["nid"]
     old_observation = old_observation[diff_nid_old_observation < time_window]
 
     return (oldest_traj, most_recent_traj), old_observation
@@ -153,21 +146,22 @@ def time_window_management(
 if __name__ == "__main__":
     import doctest
 
-    data_path = "data/month=0"
+    data_path = "../data/month=0"
     
     df_sso = load_data(data_path, "Solar System MPC")
 
-    # "1951", "53317",
+    # test case : "53317", "1951", "80343", "1196", "23101", "1758"
+    # additionnal case : "75653", "33568", "226653", "73972", "232351", "75653",
 
-    # "1584" : exemple problématique avec une time window de 14 "53317", "1951", "80343", "1196", "23101",
-
-    #  & (df_sso['nid'] >= 1610) & (df_sso['nid'] <= 1624)
+    # broken case : 73972
     specific_mpc = df_sso[
-        (df_sso["ssnamenr"].isin(["53317", "1951", "80343", "1196", "23101", "1758"]))
+        (df_sso["ssnamenr"].isin(["232351", "73972", "75653", "53317", "1951", "80343", "1196", "23101", "1758"]))
     ]
 
+    #specific_mpc = df_sso
+
     mpc_plot = (
-        specific_mpc.groupby(["ssnamenr"]).agg({"ra": list, "dec": list}).reset_index()
+        specific_mpc.groupby(["ssnamenr"]).agg({"ra": list, "dec": list, "candid": len}).reset_index()
     )
 
     all_night = np.unique(specific_mpc["nid"])
@@ -190,8 +184,8 @@ if __name__ == "__main__":
     else:
         old_observation = df_night1
 
-    time_window_limit = 14
-    verbose = False
+    time_window_limit = 16
+    verbose = True
     save_report = False
 
     for i in range(1, len(all_night)):
@@ -245,7 +239,9 @@ if __name__ == "__main__":
         ~specific_mpc["candid"].isin(traj_df["candid"])
     ]
 
-    show_results = False
+
+
+    show_results = True
 
     if show_results:  # pragma: no cover
 

alert_association/night_to_night_association.py
@@ -164,6 +164,7 @@ def angle_df(x):
     >>> np.around(np.array(res)[0], 3)
     10.305
     """
+
     ra_x, dec_x, jd_x = x[1], x[2], x[3]
 
     ra_y, dec_y, jd_y = x[6], x[7], x[8]
@@ -233,11 +234,12 @@ def cone_search_association(
     # and keep the new trajectory_id
     new_obs_assoc = new_obs_assoc.rename({"trajectory_id": "tmp_traj"}, axis=1)
     new_obs_assoc["trajectory_id"] = traj_assoc["trajectory_id"]
-
+    
     # get the two last observations of the associated trajectories in order to compute the cone search angle
     two_last = two_last_observations[
         two_last_observations["trajectory_id"].isin(traj_assoc["trajectory_id"])
     ]
+    
 
     # groupby the two last observations in order to prepare the merge with the new observations
     two_last = two_last.groupby(["trajectory_id"]).agg(
@@ -449,6 +451,7 @@ def night_to_night_trajectory_associations(
     nb_assoc_before_angle_filtering = len(new_obs_assoc)
     inter_night_obs_report["number of inter night angle filtered association"] = 0
     if len(traj_assoc) != 0:
+        
         traj_assoc, new_obs_assoc = cone_search_association(
             two_last_observations, traj_assoc, new_obs_assoc, angle_criterion
         )
@@ -771,7 +774,6 @@ def trajectory_associations(
 
     # for each trajectory nid from the last observations
     for tr_nid in trajectories_nid:
-
         current_nid_assoc_report = dict()
         current_nid_assoc_report["old nid"] = int(tr_nid)
 