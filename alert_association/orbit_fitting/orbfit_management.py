dcebd661464100ef2bb5137ab7b2ee4cbed585da
FusRoman <roman.lemontagner@gmail.com>
2021-12-14 17:51:24 +0100

    provisional designation conversion

--- alert_association/orbit_fitting/orbfit_management.py
+++ alert_association/orbit_fitting/orbfit_management.py
@@ -31,8 +31,165 @@ def band_to_str(band):
     elif band == 2:
         return "r"
 
+half_month_letter = {
+    "01": ["A", "B"],
+    "02": ["C", "D"],
+    "03": ["E", "F"],
+    "04": ["G", "H"],
+    "05": ["J", "K"],
+    "06": ["L", "M"],
+    "07": ["N", "O"],
+    "08": ["P", "Q"],
+    "09": ["R", "S"],
+    "10": ["T", "U"],
+    "11": ["V", "W"],
+    "12": ["X", "Y"],
+}
 
-def write_observation_file(filename, obs_df):
+def second_letter(i, lowercase=True):
+    """
+    Examples
+    --------
+    >>> second_letter(1)
+    'A'
+    >>> second_letter(25)
+    'Z'
+    >>> second_letter(1, False)
+    'a'
+    >>> second_letter(25, False)
+    'z'
+    >>> second_letter(9)
+    'J'
+    >>> second_letter(9, False)
+    'j'
+    """
+    if lowercase:
+        case = 64
+    else:
+        case = 96
+
+    if i <= 8:
+        return chr(i + case)
+    elif i <= 25:
+        return chr(i + case+1)
+
+def left_shift(number, n):
+    """
+    Examples
+    --------
+    >>> left_shift(152, 1)
+    15
+    >>> left_shift(14589, 3)
+    14
+    """
+    return number // 10**n
+
+def right_shift(number, n):
+    """
+    Examples
+    --------
+    >>> right_shift(123, 1)
+    3
+    >>> right_shift(0, 1)
+    0
+    >>> right_shift(1234, 2)
+    34
+    """
+    return number % 10**n
+
+def letter_cycle(cycle):
+    """
+    Examples
+    --------
+    >>> letter_cycle(10)
+    'A'
+    >>> letter_cycle(19)
+    'J'
+    >>> letter_cycle(35)
+    'Z'
+    >>> letter_cycle(44)
+    'j'
+    >>> letter_cycle(60)
+    'z'
+    """
+    cycle -= 10
+    r = (cycle-1) // 25
+    if r > 0:
+        cycle %= 25
+
+        if cycle == 0:
+            cycle = 25
+
+        return second_letter(cycle, lowercase=False)
+    else:
+        if cycle == 0:
+            cycle = 1
+        elif cycle <= 8 and cycle >= 1:
+            cycle += 1
+        return second_letter(cycle)
+
+def make_cycle(cycle):
+    """
+    Examples
+    --------
+    >>> make_cycle(0)
+    '00'
+    >>> make_cycle(10)
+    '10'
+    >>> make_cycle(108)
+    'A8'
+    >>> make_cycle(110)
+    'B0'
+    >>> make_cycle(127)
+    'C7'
+    >>> make_cycle(162)
+    'G2'
+    >>> make_cycle(193)
+    'J3'
+    >>> make_cycle(348)
+    'Y8'
+    >>> make_cycle(355)
+    'Z5'
+    >>> make_cycle(360)
+    'a0'
+    >>> make_cycle(418)
+    'f8'
+    >>> make_cycle(439)
+    'h9'
+    >>> make_cycle(440)
+    'j0'
+    """
+    if cycle <= 9:
+        return "0" + str(cycle)
+    elif cycle <= 99:
+        return str(cycle)
+    else:
+        digit = left_shift(cycle, 1)
+        unit = right_shift(cycle, 1)
+        return str(letter_cycle(digit)) + str(unit)
+
+def make_designation(time, discovery_number):
+    """
+    Examples
+    --------
+    >>> make_designation("2021-05-22 07:33:02.111", 0)
+    'K21K00A'
+    """
+    time_split = time.split(" ")[0].split("-")
+    year = time_split[0][-2:]
+
+    half_month = half_month_letter[time_split[1]]
+    if int(time_split[2]) <= 15:
+        half_month = half_month[0]
+    else:
+        half_month = half_month[1]
+    
+    order = discovery_number % 25 + 1
+    cycle = int(discovery_number / 25)
+    return "K" + year + half_month + make_cycle(cycle) + second_letter(order)
+
+
+def write_observation_file(temp_designation, obs_df):
     obs_df = obs_df.sort_values(["jd"])
     ra = obs_df["ra"]
     dec = obs_df["dec"]
@@ -51,6 +208,7 @@ def write_observation_file(filename, obs_df):
 
     t = Time(date, format="jd")
     date = t.iso
+    print([make_designation(date[0], i) for i in range(8745, 9300)])
     date = [a.split(" ") for a in date]
 
     date = [
@@ -59,17 +217,17 @@ def write_observation_file(filename, obs_df):
     ]
     res = [join_string([el1] + [el2], " ") for el1, el2 in zip(date, coord)]
     res = [
-        "     K21X05F  C"
+        "     K21X05F  C" # how the observation was made : C means CCD
         + el
         + "         "
         + str(round(mag, 1))
         + " "
         + band_to_str(b)
-        + "      I41"
+        + "      I41" # ZTF observation code
         for el, mag, b in zip(res, dcmag, band)
     ]
 
-    with open(filename + ".obs", "wt") as file:
+    with open(temp_designation + ".obs", "wt") as file:
         file.write(join_string(res, "\n"))
 
 
@@ -79,10 +237,12 @@ if __name__ == "__main__":
 
     # test1 : 2010ET42
 
-    print(df_sso.groupby(['ssnamenr']).agg({"candid":len}).sort_values(['candid']))
+    # print(df_sso.groupby(['ssnamenr']).agg({"candid":len}).sort_values(['candid']))
 
-    mpc_name = "2010ET42"
-    mpc_name = "2936"
-    mpc = df_sso[df_sso["ssnamenr"] == mpc_name]
+    # mpc_name = "2010ET42"
+    # mpc_name = "2936"
+    # mpc = df_sso[df_sso["ssnamenr"] == mpc_name]
+    # write_observation_file(mpc_name, mpc)
 
-    write_observation_file(mpc_name, mpc)
+    import doctest
+    doctest.testmod()[0]
