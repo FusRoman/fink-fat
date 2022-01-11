1cd94d0349ce388e1cad88603bd611c980565403
FusRoman <roman.lemontagner@gmail.com>
2022-01-11 14:11:16 +0100

    smaller example without mpc database

--- alert_association/ephem.py
+++ alert_association/ephem.py
@@ -127,40 +127,26 @@ if __name__ == "__main__":
     print("Load sso data")
     df_sso = load_data("Solar System MPC", nb_indirection=0).reset_index(drop=True)
 
-    one_obs = pd.DataFrame(df_sso.loc[0]).T
-    
-    print("MPC DATABASE loading, take ~10 sec")
-    t_before = t.time()
-    mpc_database = utils.get_mpc_database()
-    print("MPC DATABASE end loading, elapsed time: {}".format(t.time() - t_before))
-
-
-
-    print("Get the orbital elements of the observation", end="\n")
-    mpc_data_obs = mpc_database[mpc_database["Number"] == one_obs["ssnamenr"].values[0]]
-
-    col_select = ["ra", "dec", "ssnamenr", "jd", "nid", "objectId", "Number", "a", "e", "i", "Node", "Peri", "M"]
-    observation = one_obs.merge(mpc_data_obs, left_on="ssnamenr", right_on="Number")[col_select]
-    print(observation)
+    one_obs = df_sso[df_sso["ssnamenr"] == "795"].reset_index(drop=True).loc[0]
 
     dynamical_parameters = dict()
 
-    dynamical_parameters["ref_epoch"] = observation["jd"].values[0]
-    dynamical_parameters["semi_major_axis"] = observation["a"].values[0]
-    dynamical_parameters["eccentricity"] = observation["e"].values[0]
-    dynamical_parameters["inclination"] = observation["i"].values[0]
+    dynamical_parameters["ref_epoch"] = 2459274.6455671
+    dynamical_parameters["semi_major_axis"] = 2.7497334
+    dynamical_parameters["eccentricity"] = 0.1020056
+    dynamical_parameters["inclination"] = 19.05051
 
-    dynamical_parameters["node_longitude"] = observation["Node"].values[0]
-    dynamical_parameters["perihelion_argument"] = observation["Peri"].values[0]
-    dynamical_parameters["mean_anomaly"] = observation["M"].values[0]
+    dynamical_parameters["node_longitude"] = 17.35846
+    dynamical_parameters["perihelion_argument"] = 190.01857
+    dynamical_parameters["mean_anomaly"] = 336.48578
 
     dict_param = dict()
     dict_param["type"] = "Asteroid"
     dict_param["dynamical_parameters"] = dynamical_parameters
-    
+
     print("Write json orbital parameter on disk")
 
-    json_orb_elem_path = "aster_{}.json".format(observation["ssnamenr"].values[0])
+    json_orb_elem_path = "aster_{}.json".format(one_obs["ssnamenr"])
 
     with open(
             json_orb_elem_path, "w"
@@ -172,7 +158,7 @@ if __name__ == "__main__":
     params = {
             "-name": "",
             "-type": "Asteroid",
-            "-ep": observation["jd"].values[0],
+            "-ep": 2459274.6455671,
             "-tscale": "UTC",
             "-observer": "I41",
             "-theory": "INPOP",
@@ -193,25 +179,26 @@ if __name__ == "__main__":
     r = requests.post(url, params=params, files=files, timeout=2000)
 
     j = r.json()
+
     ephem = pd.DataFrame.from_dict(j["data"])    
     coord = SkyCoord(ephem["RA"], ephem["DEC"], unit=(u.deg, u.deg))
 
     ephem["cRA"] = coord.ra.value * 15
     ephem["cDec"] = coord.dec.value
     
-    deltaRAcosDEC = (observation["ra"] - ephem["cRA"]) * np.cos(
-        np.radians(observation["dec"].values[0])
+    deltaRAcosDEC = (one_obs["ra"] - ephem["cRA"]) * np.cos(
+        np.radians(one_obs["dec"])
     )
-    deltaDEC = observation["dec"] - ephem["cDec"]
+    deltaDEC = one_obs["dec"] - ephem["cDec"]
 
     colors = ['#15284F', '#F5622E']
     
     fig, ax = plt.subplots(
-    figsize=(10, 10), 
+    figsize=(10, 10),
     sharex=True,
-)
+    )
 
-    ax.scatter(observation['ra'], observation['dec'], label='ZTF', alpha=0.5, color=colors[1])
+    ax.scatter(one_obs['ra'], one_obs['dec'], label='ZTF', alpha=0.5, color=colors[1])
 
     ax.plot(ephem['cRA'], ephem["cDec"], ls='', color='black', marker='x', alpha=0.5, label='Ephemerides')
     ax.legend(loc='best')
@@ -228,98 +215,4 @@ if __name__ == "__main__":
     axins.set_ylabel(r'$\Delta$DEC ($^o$)')
 
     plt.show()
-
     os.remove(json_orb_elem_path)
-    exit()
-
-    n_trajectories = 1
-    n_points = 50
-
-    print("Load sso data")
-    df_sso = load_data("Solar System MPC", nb_indirection=0)
-
-    gb_ssn = df_sso.groupby(["ssnamenr"]).agg({"candid": len}).sort_values(["candid"])
-    all_track = gb_ssn[gb_ssn["candid"] == n_points].reset_index()["ssnamenr"].values
-    mpc = df_sso[df_sso["ssnamenr"].isin(all_track[:n_trajectories])][
-        ["ra", "dec", "dcmag", "fid", "jd", "nid", "candid", "ssnamenr"]
-    ]
-    all_ssnamenr = np.unique(mpc["ssnamenr"].values)
-    ssnamenr_translate = {
-        ssn: i for ssn, i in zip(all_ssnamenr, range(len(all_ssnamenr)))
-    }
-    mpc["trajectory_id"] = mpc.apply(
-        lambda x: ssnamenr_translate[x["ssnamenr"]], axis=1
-    )
-    mpc["ssnamenr"] = mpc["ssnamenr"].astype("string")
-
-    mpc = mpc.sort_values(["ssnamenr", "jd"]).reset_index(drop=True)
-
-    # print(mpc)
-
-    print("MPC DATABASE loading")
-    t_before = t.time()
-    mpc_database = utils.get_mpc_database()
-    print("MPC DATABASE end loading, elapsed time: {}".format(t.time() - t_before))
-
-    print("cross match with mpc database")
-    cross_match_mpc = mpc.merge(
-        mpc_database, how="inner", left_on="ssnamenr", right_on="Number"
-    )
-
-    cross_match_mpc["jd_ephem"] = cross_match_mpc["jd"]
-
-    print(
-        cross_match_mpc[
-            [
-                "ra",
-                "dec",
-                "jd",
-                "jd_ephem",
-                "ssnamenr",
-                "trajectory_id",
-                "a",
-                "e",
-                "i",
-                "Peri",
-                "Node",
-                "M",
-            ]
-        ]
-    )
-    print()
-    print()
-
-    ephemeris = generate_ephemeris(cross_match_mpc)
-    # ephemeris["trajectory_id"] = ephemeris["trajectory_id"].astype(int)
-
-    ephem_and_obs = ephemeris.merge(cross_match_mpc, left_on="Date", right_on="jd")
-
-    deltaRAcosDEC = (ephem_and_obs["ra"] - ephem_and_obs["cRA"]) * np.cos(
-        np.radians(ephem_and_obs["dec"])
-    )
-    deltaDEC = ephem_and_obs["dec"] - ephem_and_obs["cDec"]
-
-    colors = ['#15284F', '#F5622E']
-    
-    fig, ax = plt.subplots(
-    figsize=(10, 10), 
-    sharex=True,
-)
-
-    ax.scatter(cross_match_mpc['ra'], cross_match_mpc['dec'], label='ZTF', alpha=0.2, color=colors[1])
-
-    ax.plot(ephem_and_obs['cRA'], ephem_and_obs["cDec"], ls='', color='black', marker='x', alpha=0.2, label='Ephemerides')
-    ax.legend(loc='best')
-    ax.set_xlabel('RA ($^o$)')
-    ax.set_ylabel('DEC ($^o$)')
-
-    axins = ax.inset_axes([0.2, 0.2, 0.45, 0.45])
-
-    axins.plot( deltaRAcosDEC, deltaDEC, ls='', color=colors[0], marker='x', alpha=0.5)
-    axins.errorbar( np.mean(deltaRAcosDEC), np.mean(deltaDEC), xerr=np.std(deltaRAcosDEC), yerr=np.std(deltaDEC) )
-    axins.axhline(0, ls='--', color='black')
-    axins.axvline(0, ls='--', color='black')
-    axins.set_xlabel(r'$\Delta$RA ($^{\prime\prime}$)')
-    axins.set_ylabel(r'$\Delta$DEC ($^{\prime\prime}$)')
-
-    plt.show()
