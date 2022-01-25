594551ba0431b65fcea3423a59ea8acfe41a0801
Roman <roman.lemontagner@gmail.com>
2022-01-25 16:21:02 +0100

    add the ifort installer bash script

---
+++ ifort_installer.sh
@@ -0,0 +1,13 @@
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+
+rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+
+sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
+
+sudo apt update
+sudo apt install intel-oneapi-compiler-fortran
+
+source /opt/intel/oneapi/setvars.sh
+ifort --version
