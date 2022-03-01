f1b7a9e6ea452cddfd36ac97c1bb10034953d436
Roman <roman.lemontagner@gmail.com>
2022-03-01 15:08:12 +0100

    add Manifest file and update setup.py to work with pip

--- setup.py
+++ setup.py
@@ -2,16 +2,26 @@
 
 from setuptools import setup, find_packages
 
-print(find_packages())
+print(find_packages(where="fink_fat"))
 
 setup(
     name="fink-fat",
-    version="0.2",
+    version="0.2.2",
     description="Associations of alerts to detect moving objects",
     author="Roman Le Montagner",
     author_email="roman.le-montagner@ijclab.in2p3.fr",
     url="https://github.com/FusRoman/fink-fat",
     packages=find_packages(),
+    package_data={
+        'data': [
+            'month=*'
+        ]
+    },
     license="Apache-2.0 License",
     platforms="Linux Debian distribution",
+    project_urls={
+        'Documentation': "https://github.com/FusRoman/fink-fat/wiki",
+        "Source": "https://github.com/FusRoman/fink-fat"
+    },
+    python_requires=">=3.7",
 )
