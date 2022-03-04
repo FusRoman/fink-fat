f1cda2c284ac9b4b9139d558b4c3ecedf4603990
FusRoman <46221629+FusRoman@users.noreply.github.com>
2022-03-04 08:43:52 +0100

    Issue/66/split orbital element (#71)
    * update documentation in association.py
    
    * done the separation between association and orbfit, update the test, fix some bug
    
    * remove the wrong plot in SolarSystemObjectDatabaseExploration.ipynb
    
    * update requirement
    
    * add pragma no cover on verbose statement
    
    * pep8 requirement
    
    * update version
    
    * pep8 requirement

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-
-print(find_packages(where="fink_fat"))
+import fink_fat
 
 setup(
     name="fink-fat",
-    version="0.2.5",
+    version=fink_fat.__version__,
     description="Associations of alerts to detect moving objects",
     author="Roman Le Montagner",
     author_email="roman.le-montagner@ijclab.in2p3.fr",
     url="https://github.com/FusRoman/fink-fat",
     packages=find_packages(),
-    package_data={
-        'fink_fat': [
-            'data/month=*'
-        ]
-    },
+    package_data={"fink_fat": ["data/month=*"]},
     license="Apache-2.0 License",
     platforms="Linux Debian distribution",
     project_urls={
-        'Documentation': "https://github.com/FusRoman/fink-fat/wiki",
-        "Source": "https://github.com/FusRoman/fink-fat"
+        "Documentation": "https://github.com/FusRoman/fink-fat/wiki",
+        "Source": "https://github.com/FusRoman/fink-fat",
     },
     python_requires=">=3.7",
 )
