899e6f644a98c93c739c120a6ccb64b10db2783d
Roman <roman.lemontagner@gmail.com>
2022-06-17 10:18:42 +0200

    add licence and bash command in the orbfit and python_dependencies script

--- orbFit_installer.sh
+++ orbFit_installer.sh
@@ -1,3 +1,22 @@
+#!bin/bash
+# Copyright 2021 Le Montagner Roman
+# Author: Le Montagner Roman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+## Script to install the OrbFit software
+
+set -e
+
 ORBLOCATE=~/OrbitFit
 
 aria2c -x8 http://adams.dm.unipi.it/orbfit/OrbFit5.0.7.tar.gz
