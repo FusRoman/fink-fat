257787663e2845d3251159a7dc923812076fee5d
FusRoman <46221629+FusRoman@users.noreply.github.com>
2023-01-10 10:30:53 +0100

    Issue/110/fix assign tags test (#111)
    * fix test assign
    
    * add file test
    
    * fix ci test for the same bug

--- fink_fat/test/cli_test/test_cli.py
+++ fink_fat/test/cli_test/test_cli.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 import shutil
 import sys
+import datetime
 
 import traceback
 import logging
@@ -116,6 +117,9 @@ if __name__ == "__main__":
     obs_orb_test = pd.read_parquet(
         "{}mpc/trajectory_orb.parquet".format(data_test_path)
     )
+
+    orb_test["ssoCandId"] = orb_test["ssoCandId"].str.replace("2022", str(datetime.date.today().year), regex=False)
+    obs_orb_test["ssoCandId"] = obs_orb_test["ssoCandId"].str.replace("2022", str(datetime.date.today().year), regex=False)
 
     try:
         assert_frame_equal(
