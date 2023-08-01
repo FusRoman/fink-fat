13cd663e6e2ae70102e4e93757e8d18aa92379a4
Roman <roman.lemontagner@gmail.com>
2023-08-01 09:35:12 +0200

    add script to make the data used by the kbo and neo issue notebook

---
+++ notebook/parameters_selection/run_ff_for_kbo_neo_issue.py
@@ -0,0 +1,43 @@
+import subprocess
+import os
+import shutil
+
+
+if __name__ == "__main__":
+    from datetime import date, timedelta
+
+    start_date = date(2020, 9, 1)
+    end_date = date(2020, 10, 1)  # perhaps date.now()
+
+    delta = end_date - start_date  # returns timedelta
+
+    for i in range(delta.days + 1):
+        curr_date = start_date + timedelta(days=i)
+
+        print(curr_date)
+        ff_assoc_command = f"fink_fat associations mpc --night {curr_date} --config notebook/fink_fat_experiments/kbo_neo_issue.conf --verbose"
+        ff_orbit_command = "fink_fat solve_orbit mpc local --config notebook/fink_fat_experiments/kbo_neo_issue.conf --verbose"
+
+        results_assoc = subprocess.run(
+            ff_assoc_command, shell=True, universal_newlines=True, check=True
+        )
+
+        print()
+        print()
+
+        results_orbit = subprocess.run(
+            ff_orbit_command, shell=True, universal_newlines=True, check=True
+        )
+
+        print("move data")
+        shutil.copytree(
+            "kbo_neo_issue_ff_output/mpc", f"save_ff_output_kbo_neo_issue/{curr_date}"
+        )
+
+        print()
+        print("--- stdout ---")
+        print(results_assoc.stdout)
+        print()
+        print(results_orbit.stdout)
+        print()
+        print()
