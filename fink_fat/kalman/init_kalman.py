31ee2c15810b0e56f3a4e388456f9bb1136ea2ea
FusRoman <46221629+FusRoman@users.noreply.github.com>
2023-08-08 14:52:01 +0200

    update kalman during seeding (#141)
    * update kalman during seeding
    
    * fix orbfit local test

--- fink_fat/kalman/init_kalman.py
+++ fink_fat/kalman/init_kalman.py
@@ -5,6 +5,124 @@ import sys
 import doctest
 
 
+def make_kalman(gb_input):
+    """
+    GroupBy input
+
+    Parameters
+    ----------
+    gb_input : pd.DataFrame
+        contains a group from a groupby.
+
+    Examples
+    --------
+    >>> input = pd.DataFrame({'ra': [11, 12, 29, 21, 15], 'dec': [20, 4, 0, 17, 23], 'jd': [10, 12, 16, 24, 28], 'magpsf': [16, 12, 9, 8, 24], 'fid': [16, 27, 1, 7, 23], 'trajectory_id': [6, 6, 6, 6, 6]})
+    >>> make_kalman(input)
+    ra_0         21
+    dec_0        17
+    ra_1         15
+    dec_1        23
+    jd_0         24
+    jd_1         28
+    mag_1        24
+    fid_1        23
+    dt            4
+    vel_ra     -1.5
+    vel_dec     1.5
+    kalman     kf 6
+    dtype: object
+
+    >>> input = pd.DataFrame({'ra': [18, 19], 'dec': [1, 16], 'jd': [3, 24], 'magpsf': [13, 2], 'fid': [18, 7], 'trajectory_id': [7, 7]})
+    >>> make_kalman(input)
+    ra_0             18
+    dec_0             1
+    ra_1             19
+    dec_1            16
+    jd_0              3
+    jd_1             24
+    mag_1             2
+    fid_1             7
+    dt               21
+    vel_ra     0.047619
+    vel_dec    0.714286
+    kalman         kf 7
+    dtype: object
+    """
+
+    def compute_vel(x1, x2, dt):
+        return (x2 - x1) / dt
+
+    def make_A(dt):
+        return [
+            [1, 0, dt, 0],
+            [0, 1, 0, dt],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+
+    tr_id, ra, dec, jd, mag, fid = (
+        gb_input["trajectory_id"].values[0],
+        gb_input["ra"].values,
+        gb_input["dec"].values,
+        gb_input["jd"].values,
+        gb_input["magpsf"].values,
+        gb_input["fid"].values,
+    )
+
+    ra_0, ra_1 = ra[0], ra[1]
+    dec_0, dec_1 = dec[0], dec[1]
+    jd_0, jd_1 = jd[0], jd[1]
+    mag_1 = mag[1]
+    fid_1 = fid[1]
+
+    dt = jd_1 - jd_0
+    ra_vel = compute_vel(ra_0, ra_1, dt)
+    dec_vel = compute_vel(dec_0, dec_1, dt)
+
+    kaf = KalfAst(tr_id, ra_1, dec_1, ra_vel, dec_vel, make_A(dt))
+    if len(ra) > 2:
+        for i in range(2, len(ra)):
+            ra_i = ra[i]
+            dec_i = dec[i]
+            jd_i = jd[i]
+            mag_i = mag[i]
+            fid_i = fid[i]
+
+            ra_0 = ra_1
+            dec_0 = dec_1
+            jd_0 = jd_1
+
+            ra_1 = ra_i
+            dec_1 = dec_i
+            jd_1 = jd_i
+            mag_1 = mag_i
+            fid_1 = fid_i
+
+            dt = jd_1 - jd_0
+            ra_vel = compute_vel(ra_0, ra_1, dt)
+            dec_vel = compute_vel(dec_0, dec_1, dt)
+
+            Y = np.array([[ra_1, dec_1]])
+            kaf.update(Y, make_A(dt))
+    return pd.Series(
+        [ra_0, dec_0, ra_1, dec_1, jd_0, jd_1, mag_1, fid_1, dt, ra_vel, dec_vel, kaf],
+        index=[
+            "ra_0",
+            "dec_0",
+            "ra_1",
+            "dec_1",
+            "jd_0",
+            "jd_1",
+            "mag_1",
+            "fid_1",
+            "dt",
+            "vel_ra",
+            "vel_dec",
+            "kalman",
+        ],
+    )
+
+
 def init_kalman(
     night_pdf: pd.DataFrame,
 ) -> pd.DataFrame:
@@ -24,75 +142,33 @@ def init_kalman(
 
     Examples
     --------
-    >>> seeds = pd.DataFrame({
-    ... "ra": [0, 1, 2, 3, 4, 8],
-    ... "dec": [1, 4, 9, 12, 13, 24],
-    ... "jd": [0, 1, 2, 3, 5, 8],
-    ... "magpsf": [12, 14, 16, 17, 17.5, 18],
-    ... "fid": [1, 2, 1, 1, 2, 2],
-    ... "trajectory_id": [0, 0, 1, 1, 2, 2]
-    ... })
+    >>> np.random.seed(3)
+    >>> df = pd.DataFrame(
+    ...     np.random.randint(0, 30, size=(30, 5)),
+    ...     columns=["ra", "dec", "jd", "magpsf", "fid"],
+    ... )
+    >>> tr_id = np.repeat([0, 1, 2, 3, 4, 5, 6, 7], [2, 2, 5, 4, 8, 2, 5, 2])
+    >>> df["trajectory_id"] = tr_id
 
-    >>> init_kalman(seeds)
-       trajectory_id  ra_0  dec_0  ra_1  dec_1  jd_0  jd_1  mag_1  fid_1  dt    vel_ra   vel_dec kalman
-    0              0     0      1     1      4     0     1   14.0      2   1  1.000000  3.000000   kf 0
-    1              1     2      9     3     12     2     3   17.0      1   1  1.000000  3.000000   kf 1
-    2              2     4     13     8     24     5     8   18.0      2   3  1.333333  3.666667   kf 2
+    >>> init_kalman(df)
+       trajectory_id  ra_0  dec_0  ra_1  dec_1  jd_0  jd_1  mag_1  fid_1  dt     vel_ra   vel_dec kalman
+    0              0     8      0    10     24    21    25      3     24   4   0.500000  6.000000   kf 0
+    1              1    21     23    10     11     6    25      9     10  19  -0.578947 -0.631579   kf 1
+    2              2    26     17    10     24    26    27      7     24   1 -16.000000  7.000000   kf 2
+    3              3    20     28    11      1    23    27     15     16   4  -2.250000 -6.750000   kf 3
+    4              4     4      3    18      2    27    28      4     26   1  14.000000 -1.000000   kf 4
+    5              5    22     16     8     12     4    16     27     10  12  -1.166667 -0.333333   kf 5
+    6              6    21     17    15     23    24    28     24     23   4  -1.500000  1.500000   kf 6
+    7              7    18      1    19     16     3    24      2      7  21   0.047619  0.714286   kf 7
     """
     # remove dbscan noise
     night_pdf = night_pdf[night_pdf["trajectory_id"] != -1.0]
-
-    prep_kalman = (
-        night_pdf.sort_values("jd")
+    return (
+        night_pdf.sort_values(["trajectory_id", "jd"])
         .groupby("trajectory_id")
-        .agg(
-            ra_0=("ra", lambda x: list(x)[0] if len(list(x)) > 1 else np.nan),
-            dec_0=("dec", lambda x: list(x)[0] if len(list(x)) > 1 else np.nan),
-            ra_1=("ra", lambda x: list(x)[1]),
-            dec_1=("dec", lambda x: list(x)[1]),
-            jd_0=("jd", lambda x: list(x)[0] if len(list(x)) > 1 else np.nan),
-            jd_1=("jd", lambda x: list(x)[1]),
-            mag_1=("magpsf", lambda x: list(x)[1]),
-            fid_1=("fid", lambda x: list(x)[1]),
-        )
+        .apply(make_kalman)
         .reset_index()
     )
-    prep_kalman = prep_kalman[~prep_kalman["ra_0"].isna()]
-    prep_kalman["dt"] = prep_kalman["jd_1"] - prep_kalman["jd_0"]
-    prep_kalman["vel_ra"] = (prep_kalman["ra_1"] - prep_kalman["ra_0"]) / prep_kalman[
-        "dt"
-    ]
-    prep_kalman["vel_dec"] = (
-        prep_kalman["dec_1"] - prep_kalman["dec_0"]
-    ) / prep_kalman["dt"]
-
-    kalman_list = [
-        KalfAst(
-            tr_id,
-            ra,
-            dec,
-            ra_vel,
-            dec_vel,
-            [
-                [1, 0, dt, 0],
-                [0, 1, 0, dt],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1],
-            ],
-        )
-        for tr_id, ra, dec, ra_vel, dec_vel, dt in zip(
-            prep_kalman["trajectory_id"],
-            prep_kalman["ra_1"],
-            prep_kalman["dec_1"],
-            prep_kalman["vel_ra"],
-            prep_kalman["vel_dec"],
-            prep_kalman["dt"],
-        )
-    ]
-
-    prep_kalman["kalman"] = kalman_list
-
-    return prep_kalman
 
 
 if __name__ == "__main__":  # pragma: no cover
