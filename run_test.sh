7c4da63667fb90ca73b1d995c63be9c3d553739e
Roman <roman.lemontagner@gmail.com>
2024-07-18 11:21:23 +0200

    install fink-science branch containing the extension with fink-fat, to remove for production

--- run_test.sh
+++ run_test.sh
@@ -23,6 +23,7 @@ export PYTHONPATH="${SPARK_HOME}/python/test_coverage:$PYTHONPATH"
 export COVERAGE_PROCESS_START="${ROOTPATH}/.coveragerc"
 
 python -m pip install -U .
+pip install git+https://github.com/astrolabsoftware/fink-science.git@issue/326/roid_extension
 
 # Run the test suite
 for filename in fink_fat/associations/*.py
